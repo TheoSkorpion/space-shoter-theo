bf090123ea5d6a69615da7252992aeee3f2286a3
TheoSkorpion <83012268+TheoSkorpion@users.noreply.github.com>
2021-04-22 13:33:52 +0200

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -33,44 +33,44 @@ let projectile: Sprite = null
 let mySprite: Sprite = null
 effects.starField.startScreenEffect()
 mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . a a 4 4 
-    . . . . . . . . 6 6 6 6 a 6 6 4 
-    . . . . . 6 6 6 6 6 6 6 6 6 4 4 
-    . . . . 6 6 6 6 6 6 6 6 6 6 4 4 
-    . . 6 6 6 6 6 6 6 6 6 6 6 4 4 4 
-    . . 6 6 6 6 6 6 6 6 8 8 8 4 4 . 
-    . 6 6 6 6 6 6 6 6 8 8 6 4 4 4 . 
-    . 6 6 6 6 6 6 8 8 8 6 6 4 4 . . 
-    . . 6 6 6 6 6 6 6 6 6 6 4 . . . 
-    . . . 6 6 6 6 6 6 6 6 a 4 . . . 
-    . . . . . . 6 6 6 6 6 a . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    8 8 . . . . . . . . . . . . . . 
+    8 8 8 . . . . . . . . . . . . . 
+    8 8 8 8 . . . . . . . . . . . . 
+    8 8 8 f . . . . . . . . . . . . 
+    8 8 8 f 8 . . . . . . . . . . . 
+    8 8 a f 8 c . . . . . . . . . . 
+    a a a c 3 c 8 8 . . . . . . . . 
+    a a 3 c 3 c 3 8 8 f c f c c c c 
+    3 3 3 a 3 a 1 1 3 f 3 f b d d d 
+    3 3 3 a 1 a a a . . . . . . . . 
+    3 3 3 f a a . . . . . . . . . . 
+    3 1 1 f a . . . . . . . . . . . 
+    1 3 3 a . . . . . . . . . . . . 
+    3 3 a a . . . . . . . . . . . . 
+    a a a . . . . . . . . . . . . . 
+    a a . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
 mySprite.setFlag(SpriteFlag.StayInScreen, true)
 info.setLife(3)
 game.onUpdateInterval(2000, function () {
     EnemyShip = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . 2 2 2 2 2 2 2 4 4 4 . . . 
-        . . 2 2 f f 2 2 2 2 2 4 4 4 4 . 
-        . 2 2 f f 2 2 2 2 2 2 2 4 4 4 . 
-        2 2 f f 2 2 2 2 2 2 2 2 4 4 4 4 
-        2 f f 2 2 2 2 2 2 2 2 2 2 4 4 4 
-        2 f 2 2 2 2 2 2 2 2 2 2 2 4 4 . 
-        2 f f 2 2 2 2 2 2 2 2 2 2 4 4 4 
-        2 2 f f 2 2 2 2 2 2 2 2 4 4 4 . 
-        . 2 2 f f 2 2 2 2 2 2 2 4 4 4 . 
-        . . 2 2 f f 2 2 2 2 4 4 4 4 . . 
-        . . . . 2 2 2 2 2 4 4 . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . c c 8 . . . . 
+        . . . . . . 8 c c c f 8 c c . . 
+        . . . c c 8 8 f c a f f f c c . 
+        . . c c c f f f c a a f f c c c 
+        8 c c c f f f f c c a a c 8 c c 
+        c c c b f f f 8 a c c a a a c c 
+        c a a b b 8 a b c c c c c c c c 
+        a f c a a b b a c c c c c f f c 
+        a 8 f c a a c c a c a c f f f c 
+        c a 8 a a c c c c a a f f f 8 a 
+        . a c a a c f f a a b 8 f f c a 
+        . . c c b a f f f a b b c c 6 c 
+        . . . c b b a f f 6 6 a b 6 c . 
+        . . . c c b b b 6 6 a c c c c . 
+        . . . . c c a b b c c c . . . . 
+        . . . . . c c c c c c . . . . . 
         `, SpriteKind.Enemy)
     EnemyShip.x = scene.screenWidth()
     EnemyShip.vx = -20
